313b224b34294dcb3c95c5d18b680427a80bd5c6
Kohji Sasaya <kouji.sasaya@gmail.com>
2025-03-22 00:38:43 +0900

    fix workflow error

--- script/build.sh
+++ script/build.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ${ARTIFACT} = "linux-amd64" ]; then
-  mv ./gh-activity ./dist/
-elif [ ${ARTIFACT} = "linux-arm64" ]; then
-  mv ./gh-activity ./dist/
-elif [ ${ARTIFACT} = "windows-amd64" ]; then
-  mv ./gh-activity.bat ./dist/
-elif [ ${ARTIFACT} = "windows-arm64" ]; then
-  mv ./gh-activity.bat ./dist/
+  mv ./gh-activity ./dist/${ARTIFACT}
+elif [ ${ARTIFACT} = "windows-amd64" ]
+  mv ./gh-activity.bat ./dist/${ARTIFACT}
 fi
