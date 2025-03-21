3221d3c4cc91a09fe46e4c4a32fd8c21966986ad
Kohji Sasaya <kouji.sasaya@gmail.com>
2025-03-22 01:14:33 +0900

    update

--- script/build.sh
+++ script/build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+
+# Liux や、WSL2の場合。
 if [ ${ARTIFACT} = "linux-amd64" ]; then
   mv ./gh-activity ./dist/${ARTIFACT}
+# Windowsの場合。バッチファイルはい用意していない、動作実績は無い。
 elif [ ${ARTIFACT} = "windows-amd64" ]; then
   mv ./gh-activity.bat ./dist/${ARTIFACT}
 fi
