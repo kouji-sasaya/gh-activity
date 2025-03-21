c8eb5334924c211214e7e6b937ff47b00d07ffed
Kohji Sasaya <kouji.sasaya@gmail.com>
2025-03-22 00:42:04 +0900

    fix job error

--- script/build.sh
+++ script/build.sh
@@ -2,6 +2,6 @@
 
 if [ ${ARTIFACT} = "linux-amd64" ]; then
   mv ./gh-activity ./dist/${ARTIFACT}
-elif [ ${ARTIFACT} = "windows-amd64" ]
+elif [ ${ARTIFACT} = "windows-amd64" ]; then
   mv ./gh-activity.bat ./dist/${ARTIFACT}
 fi
