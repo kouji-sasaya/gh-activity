ea24ef0a5629b37939c563d4f2b1ada218ebad8c
Kohji Sasaya <kouji.sasaya@gmail.com>
2025-03-21 22:08:40 +0900

    add gh-actity

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-mv "target/${TARGET}/release/gh-activity" "./dist/${ARTIFACT}"
-mv "build/gh-mix-c/gh-mix-c" "./dist/${ARTIFACT}"
+#mv "target/${TARGET}/release/gh-activity" "./dist/${ARTIFACT}"
+mv "./gh-activity" "/dist/${ARTIFACT}"
